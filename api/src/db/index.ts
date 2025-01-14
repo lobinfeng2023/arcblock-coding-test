c87723ccb476747d18f65a96949dc7f6132d9d54
lobinfeng <lobinfeng@qq.com>
2025-01-14 19:33:02 +0800

    feat: 完善线上部署

--- api/src/db/index.ts
+++ api/src/db/index.ts
@@ -1,7 +1,6 @@
-// const sqlite3 = require('sqlite3').verbose();
 import sqlite3 from 'sqlite3';
 
-const db = new sqlite3.Database(__dirname + '/db.sqlite'); // 使用内存数据库，也可以指定文件路径
+const db = new sqlite3.Database(process.env.DB_PATH || './data/db.sqlite');
 
 db.serialize(() => {
   db.run('CREATE TABLE IF NOT EXISTS user (id INTEGER TEXT KEY, name TEXT, email TEXT, phone TEXT)');
--- src/env.d.ts
+++ src/env.d.ts
@@ -2,3 +2,4 @@ declare var blocklet: { prefix: string } | undefined;
 
 declare module '*.svg';
 declare module '*.png';
+declare module '*.sqlite';
